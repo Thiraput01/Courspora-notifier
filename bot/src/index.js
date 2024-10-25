893f5db294392a391f7f3e527b46dcd32207e76e
Thiraput01 <Thiraputball@gmail.com>
2024-10-26 01:25:32 +0700

    deploying

--- bot/src/index.js
+++ bot/src/index.js
@@ -4,7 +4,7 @@ const { Client, GatewayIntentBits } = require("discord.js");
 const { buildEmbedUser } = require("./buildEmbed");
 
 const app = express();
-app.use(express.json()); // For parsing application/json
+app.use(express.json());
 
 const client = new Client({
   intents: [
@@ -15,49 +15,68 @@ const client = new Client({
   ],
 });
 
-// Set up a POST route to receive course data from Python
+let targetChannels = new Map();
+
 app.post("/send-course", (req, res) => {
   const courseInfo = req.body;
-  console.log(courseInfo);
 
-  // Send the message to the Discord channel
-  sendMessageToChannel(courseInfo);
-  res.sendStatus(200); // Respond with OK
+  sendMessageToChannels(courseInfo);
+  res.sendStatus(200);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-  sendMessageToChannel({ course: "Test Course" });
-});
+function sendMessageToChannels(courseInfo) {
+  targetChannels.forEach(async (channelId, guildId) => {
+    try {
+      const channel = await client.channels.fetch(channelId);
 
-function sendMessageToChannel(courseInfo) {
-  const channelId = process.env.CHANNEL_ID; // Get the channel ID from environment variables
-
-  client.channels
-    .fetch(channelId)
-    .then(async (channel) => {
       const embed = await buildEmbedUser(courseInfo);
       await channel.send({ embeds: [embed] });
-    })
-    .catch((error) => {
-      console.error("Error fetching channel:", error);
-    });
+      console.log(
+        `Message sent to channel: ${channel.name} in guild: ${guildId}`
+      );
+    } catch (error) {
+      console.error(
+        `Error sending message to channel ${channelId} in guild ${guildId}:`,
+        error
+      );
+    }
+  });
 }
 
-client.on("messageCreate", (message) => {
+client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
-  message.reply("Yo!");
-  console.log(message.content);
+
+  const content = message.content.trim();
+
+  if (content === "!set") {
+    if (targetChannels.has(message.guild.id)) {
+      const channelId = targetChannels.get(message.guild.id);
+      const channel = await client.channels.fetch(channelId);
+      message.channel.send(
+        `A target channel has already been set at ${channel.name}.`
+      );
+      return;
+    }
+    targetChannels.set(message.guild.id, message.channel.id);
+    message.channel.send(
+      "This channel has been set as the target channel for sending messages."
+    );
+  } else if (content === "!clear") {
+    if (targetChannels.has(message.guild.id)) {
+      targetChannels.delete(message.guild.id);
+      message.channel.send("The target channel has been cleared.");
+    } else {
+      message.channel.send("No target channel has been set.");
+    }
+  }
 });
 
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
-  console.log(process.env.CHANNEL_ID);
 });
 
 client.login(process.env.TOKEN);
 
-// Start the Express server on port 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

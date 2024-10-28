42c1a8d8c949bd46d233b7be0293aff2a6309b7c
Thiraput01 <Thiraputball@gmail.com>
2024-10-28 20:02:41 +0700

    make chrome drive able to reinit itself

--- scrape/main.py
+++ scrape/main.py
@@ -1,6 +1,7 @@
 import time
 import logging
 from scraper import check_new_content, init_driver
+from selenium.common.exceptions import WebDriverException
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -8,8 +9,17 @@ if __name__ == "__main__":
     driver = init_driver()
     try:
         while True:
-            check_new_content(driver)
-            time.sleep(60)
+            try:
+                check_new_content(driver)
+                time.sleep(60)
+
+            except WebDriverException as e:
+                logging.error(f"WebDriver encountered an error: {e}")
+                logging.info("Attempting to reinitialize WebDriver...")
+                driver.quit()
+                driver = init_driver() # Reinitialize the WebDriver
+                logging.info("WebDriver reinitialized successfully.")
+                
     except KeyboardInterrupt:
         logging.info("Got KeyboardInterrupted")
         
